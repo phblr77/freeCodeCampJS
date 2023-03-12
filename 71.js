8d9376a5abca5f7b526f7cbc258511ba716d1248
phblr77 <krukd87@mail.ru>
2023-03-12 16:47:50 +0100

    Task 71

---
+++ 71.js
@@ -0,0 +1,24 @@
+function caseInSwitch(val) {
+        let answer = "";
+        switch(val){
+
+            case 1:
+                answer = "alpha";
+                console.log(answer);
+                break;
+            case 2:
+                answer = "beta";
+                console.log(answer);
+                break;
+            case 3:
+                answer = "gamma";
+                console.log(answer);
+                break;
+            case 4:
+                answer = "delta";
+                console.log(answer);
+                break;
+        }
+        return answer;
+  }
+  caseInSwitch(3);
