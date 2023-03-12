3c4f479d206ebfe6d3ff84b075349c9fc9f662ab
phblr77 <krukd87@mail.ru>
2023-03-12 15:24:08 +0100

    Task 64

---
+++ 64.js
@@ -0,0 +1,15 @@
+function testLogicalAnd(val) {
+    // Only change code below this line
+  if(val <= 50 && val >= 25){
+    // if (val) {
+    //   if (val) {
+        return "Yes";
+      }
+      return "No";
+    }
+  
+    // Only change code above this line
+    
+  
+  
+  testLogicalAnd(10);
