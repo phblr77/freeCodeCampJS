08d4aebd43d05ff38a1c7fc15975c62b7af6bbf5
phblr77 <krukd87@mail.ru>
2023-03-13 14:32:59 +0100

    Task 87

---
+++ 87.js
@@ -0,0 +1,26 @@
+const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },
+  
+    {
+      "artist": "Billy",
+      "title": "Piano",
+      "release_year": 1974,
+      "formats": [
+        "CD", 
+        "Cassette", 
+        "LP"
+      ],
+      //"gold": true
+      
+    }
+  ];
