7e3c44c2327744012fd4238e86dc2c680f333ba9
phblr77 <krukd87@mail.ru>
2023-03-16 19:25:49 +0100

    Task 131

---
+++ 131.js
@@ -0,0 +1,12 @@
+function subtract(x, y) {
+    return x - y;
+  }
+  
+export default function(x, y) {
+    return x - y;
+  }
+
+import subtract from "./math_functions.js";
+// Only change code above this line
+
+subtract(7,4);
