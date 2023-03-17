a5e0e143179ebfe650c4ce5d7b6405d6c2afd16c
phblr77 <krukd87@mail.ru>
2023-03-17 11:18:34 +0100

    Task 143

---
+++ 143.js
@@ -0,0 +1,7 @@
+// let jennyStr = "Jenny8675309";
+// let myRegex = /[a-z0-9]/ig;
+// jennyStr.match(myRegex);
+// console.log(jennyStr.match(myRegex));
+let quoteSample = "Blueberry 3.141592653s are delicious.";
+let myRegex = /[h-s2-6]/gi; // Change this line
+let result = quoteSample.match(myRegex); // Change this line
