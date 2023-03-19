9e781771a76af7656d473289ab379ffb907ac29c
phblr77 <krukd87@mail.ru>
2023-03-19 23:08:53 +0100

    Task 160

---
+++ 160.js
@@ -0,0 +1,45 @@
+let x = 7;
+let y = 9;
+let result = "to come";
+
+if(x == y) {
+  result = "Equal!";
+} else {
+  result = "Not equal!";
+}
+
+console.log(result);
+
+
+
+function getNine() {
+    let x = 6;
+    let y = 3;
+    return x + y;
+  }
+  
+  let result2 = getNine();
+  console.log(result2);
+
+
+  function raiseToPower(b, e) {
+    return Math.pow(b, e);
+  }
+  
+  let base = 2;
+  let exp = 3;
+  let power = raiseToPower(base, exp);
+  console.log(power);
+
+
+  function countToFive() {
+    let firstFive = "12345";
+    let len = firstFive.length;
+    // Only change code below this line
+    for (let i = 0; i < len; i++) {
+    // Only change code above this line
+      console.log(firstFive[i]);
+    }
+  }
+  
+  countToFive();
