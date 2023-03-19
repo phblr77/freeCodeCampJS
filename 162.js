e29a5bb99321db13c42f4ce4245421c267520a5d
phblr77 <krukd87@mail.ru>
2023-03-19 23:09:31 +0100

    Task 162

---
+++ 162.js
@@ -0,0 +1,34 @@
+let yourArray = ["Hey!", 2, 67, false, 'true']; // Change this line
+
+let myArray = ["a", "b", "c", "d"];
+// Only change code below this line
+myArray[1] = 45;
+// Only change code above this line
+console.log(myArray);
+
+
+function mixedNumbers(arr) {
+    // Only change code below this line
+    arr.unshift('I', 2, 'three');
+    arr.push(7, 'VIII', 9);
+    // Only change code above this line
+    return arr;
+  }
+  
+  console.log(mixedNumbers(['IV', 5, 'six']));
+
+
+  function popShift(arr) {
+    let popped = arr.pop(); // Change this line
+    let shifted = arr.shift(); // Change this line
+    return [shifted, popped];
+  }
+  
+  console.log(popShift(['challenge', 'is', 'not', 'complete']));
+
+
+  const arr = [2, 4, 5, 1, 7, 5, 2, 1];
+// Only change code below this line
+arr.splice(1,4);
+// Only change code above this line
+console.log(arr);
